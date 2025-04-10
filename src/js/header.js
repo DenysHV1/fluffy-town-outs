715a98cd1777ef9fe6a75e499dec9dd865829b5e
DenVik <denyagarkusha2@gmail.com>
2025-04-10 18:55:52 +0200

    fix header

--- src/js/header.js
+++ src/js/header.js
@@ -5,26 +5,27 @@ const mobileMenu = () => {
     '.header-nav-mobile-button-close'
   );
   const mobileMenuItems = document.querySelectorAll('.header-nav-mobile-item');
-const logo = document.querySelector('.header-logo')
+  const logo = document.querySelector('.header-logo');
 
   mobileMenuButton.addEventListener('click', () => {
     mobileMenu.classList.add('is-open');
-    mobileMenuButton.style.display = "none"
-    logo.style.display = "none"
+    mobileMenuButton.style.display = 'none';
+    logo.style.display = 'none';
   });
 
   mobileMenuButtonClose.addEventListener('click', () => {
     mobileMenu.classList.remove('is-open');
-    mobileMenuButton.style.display = "flex"
-    logo.style.display = "flex"
+    mobileMenuButton.style.display = 'flex';
+    logo.style.display = 'flex';
   });
 
   mobileMenuItems.forEach(item => {
     item.addEventListener('click', () => {
       mobileMenu.classList.remove('is-open');
+      mobileMenuButton.style.display = 'flex';
+      logo.style.display = 'flex';
     });
   });
-
 };
 
 export default mobileMenu;
