28006c98f629ec24d94b1be6d66ae538ad4c2509
DenVik <denyagarkusha2@gmail.com>
2025-04-10 15:35:12 +0200

    reviews

--- src/main.js
+++ src/main.js
@@ -1,5 +1,6 @@
 import mobileMenu from './js/header.js';
 import { cookiePolicy } from './js/cookiesPolicy.js';
-
+import reviewsSwiper from './js/reviews-swiper.js';
 mobileMenu();
 cookiePolicy();
+reviewsSwiper();
