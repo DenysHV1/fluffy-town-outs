33fe6d1031bdbb762186808b28aa42154308b3b9
DenVik <denyagarkusha2@gmail.com>
2025-04-10 16:08:06 +0200

    faq

--- src/js/reviews-swiper.js
+++ src/js/reviews-swiper.js
@@ -25,7 +25,7 @@ const reviewsSwiper = () => {
       },
       1440: {
         slidesPerView: 2,
-      }
+      },
     },
   });
 };
--- src/main.js
+++ src/main.js
@@ -1,6 +1,9 @@
 import mobileMenu from './js/header.js';
 import { cookiePolicy } from './js/cookiesPolicy.js';
 import reviewsSwiper from './js/reviews-swiper.js';
+import { faq } from './js/faq.js';
+
 mobileMenu();
 cookiePolicy();
 reviewsSwiper();
+faq();
