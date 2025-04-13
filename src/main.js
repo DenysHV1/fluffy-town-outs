e6944c20a86673a2ee6479d2e5afa608ea46eecc
DenVik <denyagarkusha2@gmail.com>
2025-04-13 16:51:12 +0200

    fix

--- src/main.js
+++ src/main.js
@@ -2,10 +2,8 @@ import mobileMenu from './js/header.js';
 import { cookiePolicy } from './js/cookiesPolicy.js';
 import reviewsSwiper from './js/reviews-swiper.js';
 import { faq } from './js/faq.js';
-import { gallery } from './js/gallery.js';
 
 mobileMenu();
 cookiePolicy();
 reviewsSwiper();
 faq();
-gallery()
