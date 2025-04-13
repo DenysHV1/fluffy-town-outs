69835e8c9a2e13959da3586d8480b97213700963
DenVik <denyagarkusha2@gmail.com>
2025-04-13 16:45:28 +0200

    update for gallery and clean

--- src/main.js
+++ src/main.js
@@ -2,8 +2,10 @@ import mobileMenu from './js/header.js';
 import { cookiePolicy } from './js/cookiesPolicy.js';
 import reviewsSwiper from './js/reviews-swiper.js';
 import { faq } from './js/faq.js';
+import { gallery } from './js/gallery.js';
 
 mobileMenu();
 cookiePolicy();
 reviewsSwiper();
 faq();
+gallery()
